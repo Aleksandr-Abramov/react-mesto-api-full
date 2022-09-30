1aa3ec58c73bf2318f38ed957cddab57847bbaff
Aleksandr-Abramov <ale19850505@gmail.com>
2022-09-30 22:45:14 +0300

    Подружил бек\фронт, tests 0.11

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,3 +1,4 @@
+const { SECRET_ENV, NODE_ENV } = process.env;
 const jwt = require("jsonwebtoken");
 const Unauthorized401 = require("../utils/errors/Unauthorized401");
 
@@ -8,7 +9,7 @@ const auth = (req, res, next) => {
   }
   let payload;
   try {
-    payload = jwt.verify(token, "SECRET");
+    payload = jwt.verify(token, NODE_ENV !== "production" ? SECRET_ENV : "SECRET");
   } catch (err) {
     return next(new Unauthorized401("Необходима авторизация"));
   }
